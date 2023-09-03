e2c6762a15eff22ad1ca98501be9388f21fe9b08
Fletcher Lind <fletcher.lind.dev@gmail.com>
2023-09-03 05:23:41 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# e2e-helpers
+# E2E Helpers
+
+To make the process for writing e2e-tests less tricky and more comfortable
+we have prepared a set of tools that will help you to focus on testing scenarios.
+
+- [Q Helper](docs/QHelper.md) // Provides shortcuts for css-queries
+- [R Helper](docs/RHelper.md) // Provides shortcuts for url-patterns
+
+## Helpers
+
+### Initialization
+```ts
+import { e2eHelpersFactory } from 'e2e-helpers';
+export const { q, r } = e2eHelpersFactory(/* Config */);
+```
+or
+```ts
+import { e2eHelpersFactory } from 'e2e-helpers';
+const { q, r } = e2eHelpersFactory(/* Config */);
+
+global.q = q;
+global.r = r;
+```
+
+### Configuration
+#### Default config
+```ts
+const config = {
+    customSelectorAttr: 'data-test-id',
+    customSelectorParamAttrPrefix: 'data-test-',
+    customSelectorPrefix: '%',
+    pseudoSelectorPrefix: '%%',
+    pseudoSelectorMap: {},
+}
+```
+```ts
+interface E2EHelpersConfig {
+    customSelectorAttr: string
+    customSelectorParamAttrPrefix: string
+    customSelectorPrefix: string
+    pseudoSelectorPrefix: string
+    pseudoSelectorMap: Record<string, string>
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { HelperContext, defaultHelperContext } from './helpers.context';
+import { tagDecorator, extendConfig, escape } from './inner';
+
+import { urlMatcherHelper } from './outer/url-matcher.helper';
+import { parseCustomQuery } from './outer/parse-custom-query.helper';
+
+export interface E2EHelpersConfig {
+    customSelectorAttr?: string
+    customSelectorParamAttrPrefix?: string
+    customSelectorPrefix?: string
+    pseudoSelectorPrefix?: string
+    pseudoSelectorMap?: Record<string, string>
+}
+
+export function e2eHelpersFactory(config: E2EHelpersConfig) {
+    const mergedCtx: HelperContext = extendConfig(defaultHelperContext, {
+        customSelectorAttr: config.customSelectorAttr,
+        customSelectorParamAttrPrefix: config.customSelectorParamAttrPrefix,
+        customSelectorPrefix: config.customSelectorPrefix,
+        pseudoSelectorPrefix: config.pseudoSelectorPrefix,
+        pseudoSelectorMap: config.pseudoSelectorMap
+    });
+
+    const ctx: HelperContext = {
+        ...mergedCtx,
+        _customSelectorPrefixEscaped: escape(mergedCtx.customSelectorPrefix),
+        _pseudoSelectorPrefixEscaped: escape(mergedCtx.pseudoSelectorPrefix)
+    }
+
+    return {
+        r: tagDecorator(urlMatcherHelper, ctx),
+        q: tagDecorator(parseCustomQuery, ctx),
+    }
+}
